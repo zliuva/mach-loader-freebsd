3a132a1eec7d8379f731823310bb19719c5f0645
softboysxp <softboysxp@gmail.com>
2013-11-24 18:27:00 -0500

    testcase for lazy binding

--- test/hello.c
+++ test/hello.c
@@ -1,10 +1,15 @@
 #include <stdio.h>
 
-int main(void) {
+int main(int argc, char **argv) {
 	// lazy bind here, dyld_stub_binder will be called
 	printf("Hello, World!\n");
 
 	// the second call to printf should not have to jump to dyld_stub_binder
 	printf("Hello, World!\n");
+
+	if (argc > 1) {
+		// if no additional arguments are supplied, _puts should not be bound
+		puts(argv[1]);
+	}
 }
 
