020c8d5a30bf7c14a07c02988f6da1cacea02501
softboysxp <softboysxp@gmail.com>
2013-11-24 18:20:30 -0500

    testcase update

--- test/hello.c
+++ test/hello.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 
 int main(void) {
+	// lazy bind here, dyld_stub_binder will be called
+	printf("Hello, World!\n");
+
+	// the second call to printf should not have to jump to dyld_stub_binder
 	printf("Hello, World!\n");
 }
 
